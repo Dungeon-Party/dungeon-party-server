7b33927b382e2496651ef2e9614699e8ce722df9
Richard Cox <rick.cox@ssmhealth.com>
2024-10-27 15:46:35 -0500

    removing system resource logic for the time being

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,9 @@ import { ApiKeyModule } from './api-key/api-key.module'
 import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './common/prisma/prisma.module'
 import { WinstonModule } from './common/winston/winston.module'
-import { SystemModule } from './systems/system.module'
 import { UserModule } from './users/user.module'
 
 @Module({
-  imports: [
-    WinstonModule,
-    PrismaModule,
-    AuthModule,
-    UserModule,
-    SystemModule,
-    ApiKeyModule,
-  ],
+  imports: [WinstonModule, PrismaModule, AuthModule, UserModule, ApiKeyModule],
 })
 export class AppModule {}
